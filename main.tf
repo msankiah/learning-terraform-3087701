1638892a8e9474ad9b66c4609019053d76acbea7
msankiah <147431325+msankiah@users.noreply.github.com>
2026-02-20 11:15:29 +0200

    Fix SG

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "aws_instance" "blog" {
 
 
   tags = {
-    Name = "JeanMarc"
+    Name = "blog"
   }
 }
 
@@ -63,7 +63,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http and https in. Allow everything out."
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id 
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
